c884b0f3ecb1612589c65ed7d98e702b69b6f912
jiv222 <blockiv@protonmail.com>
2020-04-02 20:39:19 -0400

    Applied lineheight patch

--- config.h
+++ config.h
@@ -10,7 +10,8 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt   = NULL;     /* -p  option; prompt to the left of input field */
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
@@ -20,8 +21,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines       = 0;    /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lineheight  = 0;    /* -h option; minimum height of a menu line */     
 
 /*
  * Characters not considered part of a word while deleting words
